39dc3385920f17ae68945bfcc4698b6d71efe4b2
API-Nice <wwwffforg@hotmail.com>
2024-10-12 16:10:48 +0800

    Update work.js

--- work.js
+++ work.js
@@ -1,18 +1,26 @@
 export default {
   async fetch(request) {
-    // 创建一个新的 URL 对象，其中包含想要转发的目标 URL
     const newUrl = new URL('https://generativelanguage.googleapis.com');
-    
-    // 如果原始请求有查询参数，我们需要将其附加到新 URL 上
     const originalUrl = new URL(request.url);
+
+    // 将查询参数从原始 URL 附加到新 URL 上
     if (originalUrl.search) {
       newUrl.search = originalUrl.search;
     }
-    
-    // 使用新的 URL 来创建一个带有正确 Host 头的新请求
-    const modifiedRequest = new Request(newUrl.toString(), request); // 这会复制原始请求的所有属性，包括 headers 和 body
-    
+
+    // 创建一个新的 Headers 对象，复制原有的 headers 并添加 Host 头
+    const headers = new Headers(request.headers);
+    headers.set('Host', newUrl.host);
+
+    // 创建一个带有正确 Host 头的新请求
+    const modifiedRequest = new Request(newUrl.toString(), {
+      method: request.method,
+      headers: headers,
+      body: request.body,
+      redirect: 'follow' // 确保跟随重定向
+    });
+
     // 发送修改后的请求
-    return fetch(modifiedRequest, { method: request.method, body: request.body });
+    return fetch(modifiedRequest);
   },
 };
